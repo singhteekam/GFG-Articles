966e3be5ce5308c89675d05cc43eac83da5c830b
Teekam Singh <teekams10999@gmail.com>
2020-08-27 10:11:11 +0530

    first commit

---
+++ textspan_in_flutter/lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'TextSpan',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('GeeksforGeeks'),
+        backgroundColor: Colors.green
+        ),
+        body: Center(
+          child: Text.rich(
+            TextSpan(
+              text: 'This is textspan ',
+              children: <InlineSpan>[
+                TextSpan(
+                  text: 'Widget in flutter',
+                  style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold),
+                )
+              ]
+            )
+          ),
+        ),
+        backgroundColor: Colors.lightBlue[50],
+    );
+  }
+}
